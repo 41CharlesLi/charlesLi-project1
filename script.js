b49c39e0d86921d6f9733b5a62d9f7ddab306cb1
Charles <charlesli41@gmail.com>
2022-07-10 09:26:05 -0400

    made hamburger more accessible

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 function showMenu() {
     const menuBox = document.querySelector("ul");
+    const linksArray = menuBox.children;
+    const shoppingCounter = linksArray[7].lastElementChild.lastElementChild;
 
     menuBox.classList.toggle("menuBox");
-    console.log(menuBox.classList);
+
+    Array.from(linksArray).forEach((listItem) => {
+        const navLink = listItem.firstElementChild;
+        navLink.classList.toggle("mobile-hidden");
+    });
+    shoppingCounter.classList.toggle("mobile-hidden");
 }
 
-var imgList = document.getElementById("imgList");
-var scrollRight = document.getElementById("leftScroll");
-var scrollLeft = document.getElementById("rightScroll");
-
+const imgList = document.getElementById("imgList");
+const scrollRight = document.getElementById("rightScroll");
+const scrollLeft = document.getElementById("leftScroll");
+const indImg = document.querySelector(".circleGalleryImg");
+// console.log(indImg.offsetParent.nextElementSibling);
+// console.log(imgList);
+// console.log(indImg);
 // When a user clicks on the right arrow, the ul will scroll 750px to the right
 scrollRight.addEventListener("click", (event) => {
-    if (window.innerWidth < 700) {
-        imgList.scrollBy(-165, 0);
-    } else {
-        imgList.scrollBy(-600, 0);
-    }
+    imgList.scrollBy(indImg.width, 0);
 });
 
 // When a user clicks on the left arrow, the ul will scroll 750px to the left
 scrollLeft.addEventListener("click", (event) => {
-    if (window.innerWidth < 700) {
-        imgList.scrollBy(165, 0);
-    } else {
-        imgList.scrollBy(600, 0);
-    }
+    imgList.scrollBy(-indImg.width, 0);
 });
